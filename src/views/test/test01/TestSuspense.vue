685c380ae21c0ead87fc8fe2ffbdd5ae6dc65521
‘15575602407’ <15575694746@qq.com>
2023-02-09 11:55:23 +0800

    2023.02.09.003

--- src/views/test/test01/TestSuspense.vue
+++ src/views/test/test01/TestSuspense.vue
@@ -1,24 +1,39 @@
 <template>
   <div>
     <h2>延时出结果异步</h2>
-    <h1>result:{{ result }}</h1>
+    <img :src="state.message" alt="" />
+    <el-button type="primary" @click="getRawData">获取图片</el-button>
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent,ref } from "vue";
+<script setup lang="ts">
+import { defineComponent, ref, reactive,toRefs } from "vue";
+import axios from "axios";
 
-export default defineComponent({
-  setup() {
-    return new Promise((resolve) => {
-        setTimeout(() => {
-        return resolve({
-          result: 100,
-        });
-      }, 3000);
-    });
-  },
+// 数据长什么样
+interface DogResult {
+  message: string;
+  status: string;
+}
+
+// 定义初始数据值和类型
+const state:DogResult = reactive({
+  message: "",
+  status: "",
 });
+
+// 把它们变成响应式
+const RefState = toRefs(state)
+
+// 写方法和逻辑
+const getRawData = async () => {
+  await axios.get("https://dog.ceo/api/breeds/image/random").then((res) => {
+    // 拿到回来的数据，重新赋值
+    return state.message = res.data.message
+  });
+};
+
+
 </script>
 
 <style scoped></style>
