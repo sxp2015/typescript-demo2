29483d4f5c8216ad4b67a120d15a57fcbecc78b3
‘15575602407’ <15575694746@qq.com>
2023-02-15 13:21:33 +0800

    2023.02.15.002

--- src/views/artics/Index.vue
+++ src/views/artics/Index.vue
@@ -6,7 +6,7 @@
 </template>
 
 <script setup lang="ts">
-import { IColumnProps } from '../../types/columnList';
+import {testData,testPost } from '../../types/testData'
 import { IUserProps } from '../../types/userProps';
 import GlobalHead from '../../components/GlobalUserProps.vue';
 import ColumnList from '../../components/ColumnList.vue';
@@ -17,36 +17,7 @@ const testUserProps: IUserProps = reactive({
   userName: 'Sunny',
   userID: 5709
 })
-const testData: IColumnProps[] = [
-  {
-    id: 1,
-    title: "测试标题数据1",
-    description: "测试描述方面的内容1，需要多一点文本，才能测试出效果",
-    avatar:
-      "https://t3.lw05.cn/static/images/index/logo.png",
-  },
-  {
-    id: 2,
-    title: "测试标题数据2",
-    description: "测试描述方面的内容2，需要多一点文本，才能测试出效果",
-    avatar:
-      "https://t3.lw05.cn/static/images/index/logo.png",
-  },
-  {
-    id: 3,
-    title: "测试标题数据3",
-    description: "测试描述方面的内容3，需要多一点文本，才能测试出效果",
-    avatar:
-      "https://t3.lw05.cn/static/images/index/logo.png",
-  },
-  {
-    id: 4,
-    title: "测试标题数据4",
-    description: "测试描述方面的内容4，需要多一点文本，才能测试出效果",
-    avatar:
-      "https://t3.lw05.cn/static/images/index/logo.png",
-  },
-];
+
 </script>
 
 <style scoped>
